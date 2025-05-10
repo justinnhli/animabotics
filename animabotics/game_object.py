5e35a8efb30247f47b23b5a24b486e7e83cf2263
Justin Li <justinnhli@gmail.com>
2025-05-10 14:56:34 -0700

    [game-object] slightly simplify Newtonian motion calculations

--- animabotics/game_object.py
+++ animabotics/game_object.py
@@ -173,10 +173,9 @@ class PhysicsObject(GameObject):
         # type: (int, int) -> None
         """Update the velocity and the position."""
         super().update(elapsed_msec, elapsed_msec_squared)
-        prev_velocity = self.velocity
+        if self.velocity or self.acceleration:
+            self.move_by(self.velocity * elapsed_msec + 0.5 * self.acceleration * elapsed_msec_squared)
+        if self.angular_velocity or self.angular_acceleration:
+            self.rotate_by(self.angular_velocity * elapsed_msec + 0.5 * self.angular_acceleration * elapsed_msec_squared)
         self.velocity += self.acceleration * elapsed_msec
         self.angular_velocity += self.angular_acceleration * elapsed_msec
-        if self.velocity:
-            self.move_by(prev_velocity * elapsed_msec + 0.5 * self.acceleration * elapsed_msec_squared)
-        if self.angular_velocity:
-            self.rotate_by(self.angular_velocity * elapsed_msec)
