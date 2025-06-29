0c9e3b816b3be0c427c124479ca6d01e72c988fa
Justin Li <justinnhli@gmail.com>
2025-06-29 10:01:16 -0700

    [tests] include demos in coverage report

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 # set python3 executable
 PYTHON3=$HOME/.local/share/venv/animabotics/bin/python3
 # run tests and generate coverage report if all tests passed
-$PYTHON3 -m coverage run --branch --source=animabotics -m pytest -v "$@" \
+$PYTHON3 -m coverage run --branch --source=animabotics,demos -m pytest -v "$@" \
     && $PYTHON3 -m coverage html
