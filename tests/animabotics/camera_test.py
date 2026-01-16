77601044e48147eb108adb46c591eaf38b79312c
Justin Li <justinnhli@gmail.com>
2026-01-16 09:45:41 -0800

    [camera] add test for zoom level changes

--- tests/animabotics/camera_test.py
+++ tests/animabotics/camera_test.py
@@ -24,3 +24,28 @@ def test_canvas_rect():
     camera = Camera(canvas)
     camera.draw_geometry(Polygon.rectangle(2, 2))
     check_image(canvas.image, 'canvas__rect_outline.ppm')
+
+
+def test_camera_collision_geometry():
+    canvas = Canvas(100, 100, 'test')
+    camera = Camera(canvas)
+    assert camera.collision_geometry == Polygon((
+        Point2D(-50, 50),
+        Point2D(-50, -50),
+        Point2D(50, -50),
+        Point2D(50, 50),
+    ))
+    camera.zoom_level = 1
+    assert camera.collision_geometry == Polygon((
+        Point2D(-40, 40),
+        Point2D(-40, -40),
+        Point2D(40, -40),
+        Point2D(40, 40),
+    ))
+    camera.zoom_level = -1
+    assert round(camera.collision_geometry, 1) == round(Polygon((
+        Point2D(-62.5, 62.5),
+        Point2D(-62.5, -62.5),
+        Point2D(62.5, -62.5),
+        Point2D(62.5, 62.5),
+    )), 1)
