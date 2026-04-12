7a69f65724802c0973e9de0a2c8e335306337899
Justin Li <justinnhli@gmail.com>
2026-04-12 09:28:45 -0700

    [tests] fix testing order
    this was screwed up due to the use of subdirectories

--- conftest.py
+++ conftest.py
@@ -15,23 +15,35 @@ def pytest_collection_modifyitems(session, config, items): # pylint: disable = u
     # group the tests by their paths
     num_items = len(items)
     tests = defaultdict(list)
+    animabotics_test_path = SOURCE_PATH.parent / 'tests' / 'animabotics'
     for item in items:
         test_path_str, *_ = item.reportinfo()
-        module = Path(test_path_str).stem.replace('_test', '')
+        test_path = Path(test_path_str)
+        if animabotics_test_path in test_path.parents:
+            module = Path(test_path_str).relative_to(animabotics_test_path)
+            module = module.with_stem(module.stem.replace('_test', ''))
+        else:
+            module = test_path
         tests[module].append(item)
     # collect the importees of of the source files
     modules = set()
     importees_of = defaultdict(set)
     importers_of = defaultdict(set)
-    for source_path in SOURCE_PATH.glob('*.py'):
-        importer = source_path.stem
+    for source_path in SOURCE_PATH.glob('**/*.py'):
+        importer = source_path.relative_to(SOURCE_PATH)
         modules.add(importer)
         with source_path.open(encoding='utf-8') as fd:
             for line in fd:
-                match = re.fullmatch(r'from \.([a-z0-9_]*) import .*', line.strip())
+                match = re.fullmatch(r'from \.(\.*)([a-z0-9_]*) import .*', line.strip())
                 if not match:
                     continue
-                importee = match.group(1)
+                parent = source_path.parents[len(match.group(1))]
+                if (parent / match.group(2)).is_dir():
+                    importee_path = parent / match.group(2) / '__init__.py'
+                else:
+                    importee_path = parent / (match.group(2) + '.py')
+                assert importee_path.is_file
+                importee = importee_path.relative_to(SOURCE_PATH)
                 importees_of[importer].add(importee)
                 importers_of[importee].add(importer)
     # re-add tests in topological order
@@ -46,6 +58,7 @@ def pytest_collection_modifyitems(session, config, items): # pylint: disable = u
             importees_of[importer].remove(importee)
             if not importees_of[importer]:
                 newly_ready.add(importer)
+                del importees_of[importer]
         queue.extend(sorted(newly_ready))
     # add any non-matching tests at the end
     for value in tests.values():
