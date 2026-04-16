caefc70d9b80dbf4cbc4750189b4553f47788d57
Justin Li <justinnhli@gmail.com>
2026-04-16 14:49:45 -0700

    [utilitypes] add MaybeSequence

--- animabotics/utilitypes.py
+++ animabotics/utilitypes.py
@@ -1,7 +1,8 @@
 """Abstract types."""
 
-from typing import TypeVar, Protocol, Any
+from typing import TypeVar, Protocol, Any, Union, overload
 
+T = TypeVar('T')
 T_contra = TypeVar("T_contra", contravariant=True)
 
 
@@ -15,3 +16,34 @@ class ComparableT(Protocol[T_contra]):
     def __lt__(self, __other):
         # type: (T_contra) -> bool
         ...
+
+
+MaybeSequence = Union[list[T], tuple[T, ...], T]
+
+
+@overload
+def unwrap_maybe_sequence(arg: list[T]) -> tuple[T, ...]:
+    # pylint: disable = unused-argument
+    ...
+
+
+@overload
+def unwrap_maybe_sequence(arg: tuple[T, ...]) -> tuple[T, ...]:
+    # pylint: disable = unused-argument
+    ...
+
+
+@overload
+def unwrap_maybe_sequence(arg: T) -> tuple[T, ...]:
+    # pylint: disable = unused-argument
+    ...
+
+
+def unwrap_maybe_sequence(arg: MaybeSequence[T]) -> tuple[T, ...]:
+    """Convert either a sequence or a naked element into a tuple."""
+    if isinstance(arg, tuple):
+        return arg
+    elif isinstance(arg, list):
+        return tuple(arg)
+    else:
+        return (arg, )
