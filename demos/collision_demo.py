d72ec99f9fc175f037bd579e72ba2f319a37074f
Justin Li <justinnhli@gmail.com>
2026-04-16 14:49:45 -0700

    [demos] fix collision demo

--- demos/collision_demo.py
+++ demos/collision_demo.py
@@ -6,50 +6,57 @@
 import sys
 from random import Random
 from pathlib import Path
+from typing import Sequence
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from animabotics import Game
-from animabotics import GameObject, PhysicsObject
 from animabotics import Polygon
 from animabotics import Point2D, Vector2D
-from animabotics import Collidable
-from animabotics import AnimationController, Shape
+from animabotics import Collidable, Newtonian, Unanimated
+from animabotics import Shape
 
 
-class Ball(PhysicsObject, Collidable):
+class Ball(Collidable, Newtonian, Unanimated):
     """A bouncy ball."""
 
     RADIUS = 15
     ELLIPSE = Polygon.ellipse(RADIUS, RADIUS)
     SHAPE = Shape(ELLIPSE)
 
-    def __init__(self):
-        # type: () -> None
-        super().__init__(Ball.ELLIPSE)
-        self.animation = AnimationController.create_static_animation(Ball.SHAPE)
+    def __init__(self, position, velocity):
+        # type: (Point2D, Vector2D) -> None
+        super().__init__(
+            position=position,
+            velocity=velocity,
+            mass=1,
+            physics_geometry=Ball.ELLIPSE,
+            sprite_or_shapes=Shape(Ball.ELLIPSE),
+            collision_groups=['balls'],
+        )
 
     def bounce_vertical(self, _):
-        # type: (GameObject) -> None
+        # type: (Collidable) -> None
         """Bounce the ball vertically."""
         self.velocity = Vector2D.from_matrix(self.velocity.matrix.y_reflection)
 
     def bounce_horizontal(self, _):
-        # type: (GameObject) -> None
+        # type: (Collidable) -> None
         """Bounce the ball horizontally."""
         self.velocity = Vector2D.from_matrix(self.velocity.matrix.x_reflection)
 
 
-class Wall(GameObject):
+class Wall(Collidable, Unanimated):
     """A boundary wall."""
 
-    def __init__(self, width, height):
-        # type: (int, int) -> None
-        super().__init__(Polygon.rectangle(width, height))
-        self.width = width
-        self.height = height
-        self.animation = AnimationController.create_static_animation(
-            Shape(self.collision_geometry),
+    def __init__(self, width, height, position, collision_groups):
+        # type: (int, int, Point2D, Sequence[str]) -> None
+        geometry = Polygon.rectangle(width, height)
+        super().__init__(
+            position=position,
+            sprite_or_shapes=Shape(geometry),
+            physics_geometry=geometry,
+            collision_groups=collision_groups,
         )
 
 
@@ -73,37 +80,43 @@ class CollisionDemo(Game):
         window_height = self.window_height - (wall_thickness // 2)
         # top and bottom walls
         for indicator in (-1, 1):
-            wall = Wall(window_width + 2 * wall_thickness, wall_thickness)
-            wall.add_to_collision_group('h_walls')
-            wall.move_to(Point2D(0, indicator * (window_height + wall_thickness) // 2))
-            self.add_object(wall)
+            wall = Wall(
+                width=window_width + 2 * wall_thickness,
+                height=wall_thickness,
+                position=Point2D(0, indicator * (window_height + wall_thickness) // 2),
+                collision_groups=['h_walls'],
+            )
+            self.add_entity(wall)
         # left and right walls
         for indicator in (-1, 1):
-            wall = Wall(wall_thickness, window_height)
-            wall.add_to_collision_group('v_walls')
-            wall.move_to(Point2D(indicator * (window_width + wall_thickness) // 2, 0))
-            self.add_object(wall)
+            wall = Wall(
+                width=wall_thickness,
+                height=window_height,
+                position=Point2D(indicator * (window_width + wall_thickness) // 2, 0),
+                collision_groups=['v_walls'],
+            )
+            self.add_entity(wall)
         # add the balls
         area_width = window_width - wall_thickness - Ball.RADIUS
         area_height = window_height - wall_thickness - Ball.RADIUS
         for _ in range(self.num_balls):
-            ball = Ball()
-            ball.add_to_collision_group('balls')
-            ball.move_to(Point2D(
-                self.rng.randrange(
-                    -(area_width // 2),
-                    (area_width // 2) + 1,
+            ball = Ball(
+                position=Point2D(
+                    self.rng.randrange(
+                        -(area_width // 2),
+                        (area_width // 2) + 1,
+                    ),
+                    self.rng.randrange(
+                        -(area_height // 2),
+                        (area_height // 2) + 1,
+                    ),
                 ),
-                self.rng.randrange(
-                    -(area_height // 2),
-                    (area_height // 2) + 1,
+                velocity=Vector2D(
+                    self.rng.randrange(-5, 6) / 100,
+                    self.rng.randrange(-5, 6) / 100,
                 ),
-            ))
-            ball.velocity = Vector2D(
-                self.rng.randrange(-5, 6) / 100,
-                self.rng.randrange(-5, 6) / 100,
             )
-            self.add_object(ball)
+            self.add_entity(ball)
 
     def register_collisions(self):
         # type: () -> None
@@ -116,7 +129,7 @@ class CollisionDemo(Game):
 def main(): # pragma: no cover
     # type: () -> None
     """Provide a CLI entry point."""
-    CollisionDemo(20).start()
+    CollisionDemo(20, random_seed=8675309).start()
 
 
 if __name__ == '__main__': # pragma: no cover
